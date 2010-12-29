74d6d1a3a1f03e5fcf95707ec81ca83a937e319a
James Tennant <james@artificialstate.com>
2010-12-29 14:31:47 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c9885097137c87f5d0f0ff838c6f1ce69c64979cad4f3945b485129f7ddef6273b571f978eda710b9dd972e2f3fc4866d4deb022215da1531d57be000173f02a'
