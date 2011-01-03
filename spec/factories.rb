635d58b048b9b2828f9f5cf2480a0f5adde9e4b4
James Tennant <james@artificialstate.com>
2011-01-03 13:20:17 +0000

    User model with passwords and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "James"
+  user.email                 "james@test.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
